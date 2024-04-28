f774909a04792822ec39a050247a1891f1278983
ase12345636 <ase12345636@gmail.com>
2024-04-28 23:39:02 +0800

    Again some self easy problem in the other language. 20240428

---
+++ Kattis/Hipp_hipp_hurra.java
@@ -0,0 +1,10 @@
+public class Hipp_hipp_hurra
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            System.out.println("Hipp hipp hurra!");
+        }
+    }
+}
